48a1dd562fef0a5b6be48aef70502cccb52a46f5
Matt Zabriskie <mzabriskie@apple.com>
2019-04-16 01:15:42 -0600

    Fixing memory leak

--- app/components/Tickets.js
+++ app/components/Tickets.js
@@ -8,11 +8,15 @@ import Countdown from 'components/Countdown';
 const TICKET_RELEASE_TIME = moment.utc(constants.Dates.TICKET_RELEASE);
 
 export default class Tickets extends Component {
+  componentWillUnmount() {
+    clearTimeout(this.__timer);
+  }
+
   render() {
     const ticketsAvailable = moment.utc().isSameOrAfter(TICKET_RELEASE_TIME);
 
     if (!ticketsAvailable) {
-      setTimeout(this.forceUpdate.bind(this), 1000);
+      this.__timer = setTimeout(this.forceUpdate.bind(this), 1000);
     }
 
     return (
